e404248d751ed8485da763a5e611c89ec6cfa06b
Qulip <77991314+Qulip@users.noreply.github.com>
2021-07-12 16:38:50 +0900

    add 11day study

---
+++ 11day/q10.js
@@ -0,0 +1,3 @@
+for(var i=10; i>=0; i-=3){
+    alert(i);
+}
---
+++ 11day/q15.js
@@ -0,0 +1,8 @@
+var rst = "";
+for(var i = 1; i<=10; i++){
+    for(var j=1; j<=i; j++){
+        rst = rst+"*";
+    }
+    rst = rst+"\n";
+}
+alert(rst);
